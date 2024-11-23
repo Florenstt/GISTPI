5e674bf427d5dc1b0289f0c4fdd440a15d1f7311
florensg <floo.g1096@ca.frre.utn.edu.ar>
2024-11-23 00:26:43 -0300

    Se pueden habilitar y deshabilitar las capas
    Aun se esta trabajando con una sola capa (ActividadesEconomicas)

--- js/map.js
+++ js/map.js
@@ -1,4 +1,14 @@
-var Actividades_Economicas = new ol.layer.Image({
+var baseMapLayer = new ol.layer.Tile({
+    title: "Base Map",
+    source: new ol.source.TileWMS({
+        url: 'https://wms.ign.gob.ar/geoserver/ows',
+        params: {
+            LAYERS: 'capabaseargenmap'
+        }
+    })
+});
+
+var actividadesEconomicasLayer = new ol.layer.Image({
     title: "Actividades Económicas",
     visible: true,
     source: new ol.source.ImageWMS({
@@ -11,20 +21,12 @@ var Actividades_Economicas = new ol.layer.Image({
     })
 });
 
-// Definición del mapa y su capa
+// Definición del mapa y sus capas
 var map = new ol.Map({
     target: 'map',
     layers: [
-        new ol.layer.Tile({
-            title: "Base Map",
-            source: new ol.source.TileWMS({
-                url: 'https://wms.ign.gob.ar/geoserver/ows',
-                params: {
-                    LAYERS: 'capabaseargenmap'
-                }
-            })
-        }),
-        Actividades_Economicas
+        baseMapLayer,
+        actividadesEconomicasLayer
     ],
     view: new ol.View({
         projection: 'EPSG:4326',
@@ -33,5 +35,38 @@ var map = new ol.Map({
     })
 });
 
+// Manejar los checkboxes para habilitar/deshabilitar capas
+var checkboxBaseMap = document.getElementById('toggleBaseMap');
+checkboxBaseMap.addEventListener('change', function () {
+    var checked = this.checked;
+    if (checked !== baseMapLayer.getVisible()) {
+        baseMapLayer.setVisible(checked);
+    }
+});
+
+baseMapLayer.on('change:visible', function () {
+    var visible = this.getVisible();
+    if (visible !== checkboxBaseMap.checked) {
+        checkboxBaseMap.checked = visible;
+    }
+});
+
+var checkboxActividadesEconomicas = document.getElementById('toggleActividadesEconomicas');
+checkboxActividadesEconomicas.addEventListener('change', function () {
+    var checked = this.checked;
+    if (checked !== actividadesEconomicasLayer.getVisible()) {
+        actividadesEconomicasLayer.setVisible(checked);
+    }
+});
+
+actividadesEconomicasLayer.on('change:visible', function () {
+    var visible = this.getVisible();
+    if (visible !== checkboxActividadesEconomicas.checked) {
+        checkboxActividadesEconomicas.checked = visible;
+    }
+});
+
+ 
+
 // Mostrar el nombre de la capa en la barra lateral
-document.getElementById('layer-name').innerText = Actividades_Economicas.get('title');
+document.getElementById('layer-name').innerText = actividadesEconomicasLayer.get('title');
