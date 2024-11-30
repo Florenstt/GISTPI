5eb369f9c1f60ce8569cfed938507b02e6b65b8f
florensg <floo.g1096@ca.frre.utn.edu.ar>
2024-11-30 10:57:34 -0300

    Sidebar se fue

--- tp-final/src/App.js
+++ tp-final/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Sidebar from './componentes/Sidebar';
 import './App.css';
 import MapComponent from './componentes/MapComponent';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
       <div className="content">
         {/* Aquí va el contenido principal de tu aplicación */}
         <h1>Contenido Principal</h1>
