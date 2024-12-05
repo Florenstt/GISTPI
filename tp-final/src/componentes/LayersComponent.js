a840e9b388a9fdb1a0c94d1ff72219a42fbadec3
florensg <floo.g1096@ca.frre.utn.edu.ar>
2024-12-05 12:36:44 -0300

    muestra cada uno de los iconos

--- tp-final/src/componentes/LayersComponent.js
+++ tp-final/src/componentes/LayersComponent.js
@@ -8,37 +8,43 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
 import Style from 'ol/style/Style';
-import Fill from 'ol/style/Fill';
-import Stroke from 'ol/style/Stroke';
-import CircleStyle from 'ol/style/Circle';
+import Icon from 'ol/style/Icon';
 import { fromLonLat } from 'ol/proj';
+import actividadesAgropecuariasIcon from '../assets/icons/actividadesAgropecuarias.png'; // Importa el archivo PNG por defecto
+import actividadesEconomicasIcon from '../assets/icons/actividadesEconomicas.png';
+import complejoEnergiaIcon from '../assets/icons/ComplejoDeEnergiaEne.png';
+import defaultIcon from '../assets/icons/default.png'; // Importa un ícono por defecto
 import './MapComponent.css';
 
 export const createLayers = () => {
-  const redCircleStyle = new Style({
-    image: new CircleStyle({
-      radius: 5,
-      fill: new Fill({ color: 'red' }),
-      stroke: new Stroke({ color: 'black', width: 1 }), // Borde del círculo
-    }),
-  });
+  const iconMapping = {
+    'actividades_agropecuarias': actividadesAgropecuariasIcon,
+    'actividades_economicas': actividadesEconomicasIcon,
+    'complejo_de_energia': complejoEnergiaIcon,
+    // Añade más mapeos según sea necesario
+  };
 
-  const createVectorLayer = (title, typeName, color, geometryType) => {
+  const createVectorLayer = (title, typeName, geometryType) => {
+    const vectorSource = new VectorSource({
+      url: `http://localhost:8080/geoserver/TPI/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=TPI:${typeName}&outputFormat=application/json`,
+      format: new GeoJSON(),
+    });
+
+    const iconSrc = iconMapping[typeName] || defaultIcon; // Usa el ícono correspondiente o el ícono por defecto
+
+    const pngStyle = new Style({
+      image: new Icon({
+        src: iconSrc, // Usa el PNG importado
+        scale: 0.3, // Ajusta el tamaño del PNG
+      }),
+    });
+
     return new VectorLayer({
       title,
       visible: false, // Las capas están ocultas por defecto
       geometryType, // Añadimos el tipo de geometría
-      source: new VectorSource({
-        url: `http://localhost:8080/geoserver/TPI/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=TPI:${typeName}&outputFormat=application/json`,
-        format: new GeoJSON(),
-      }),
-      style: new Style({
-        image: new CircleStyle({
-          radius: 5,
-          fill: new Fill({ color }),
-          stroke: new Stroke({ color: 'black', width: 1 }),
-        }),
-      }),
+      source: vectorSource,
+      style: pngStyle,
       styleUrl: `http://localhost:8080/geoserver/TPI/wms?service=WMS&request=GetLegendGraphic&version=1.1.1&format=image/png&layer=TPI:${typeName}`,
     });
   };
@@ -47,6 +53,7 @@ export const createLayers = () => {
     return new TileLayer({
       title,
       visible: false, // Las capas están ocultas por defecto
+      geometryType: 'Polygon', // Añadimos el tipo de geometría
       source: new TileWMS({
         url: 'http://localhost:8080/geoserver/TPI/wms',
         params: {
@@ -54,61 +61,62 @@ export const createLayers = () => {
           TILED: true
         },
         serverType: 'geoserver'
-      })
+      }),
+      styleUrl: `http://localhost:8080/geoserver/TPI/wms?service=WMS&request=GetLegendGraphic&version=1.1.1&format=image/png&layer=TPI:${typeName}`,
     });
   };
 
   const pointLayers = [
-    createVectorLayer('Actividades Agropecuarias', 'actividades_agropecuarias', 'red', 'Point'),
-    createVectorLayer('Actividades Económicas', 'actividades_economicas', 'blue', 'Point'),
-    createVectorLayer('Complejo de Energía', 'complejo_de_energia', 'green', 'Point'),
-    createVectorLayer('Edif Construcciones Turísticas', 'edif_construcciones_turisticas', 'yellow', 'Point'),
-    createVectorLayer('Edif Depor y Esparcimiento', 'edif_depor_y_esparcimiento', 'purple', 'Point'),
-    createVectorLayer('Edif Educación', 'edif_educacion', 'orange', 'Point'),
-    createVectorLayer('Edif Religiosos', 'edif_religiosos', 'pink', 'Point'),
-    createVectorLayer('Edificio de Seguridad', 'edificio_de_seguridad', 'brown', 'Point'),
-    createVectorLayer('Edificio Público', 'edificio_publico', 'cyan', 'Point'),
-    createVectorLayer('Edificios Ferroviarios', 'edificios_ferroviarios', 'magenta', 'Point'),
-    createVectorLayer('Edificio de Salud', 'edificio_de_salud', 'lime', 'Point'),
-    createVectorLayer('Estructuras Portuarias', 'estructuras_portuarias', 'navy', 'Point'),
-    createVectorLayer('Infraest Aeroportuaria Punto', 'infraest_aeroportuaria_punto', 'teal', 'Point'),
-    createVectorLayer('Infraestructura Hidro', 'infraestructura_hidro', 'olive', 'Point'),
-    createVectorLayer('Localidad', 'localidad', 'maroon', 'Point'),
-    createVectorLayer('Marcas y Señales', 'marcas_y_senales', 'silver', 'Point'),
-    createVectorLayer('Obra Portuaria', 'obra_portuaria', 'gold', 'Point'),
-    createVectorLayer('Otras Edificaciones', 'otras_edificaciones', 'coral', 'Point'),
-    createVectorLayer('Puente Red Vial Punto', 'puente_red_vial_punto', 'orchid', 'Point'),
-    createVectorLayer('Puntos de Alturas Topográficas', 'puntos_de_alturas_topograficas', 'plum', 'Point'),
-    createVectorLayer('Puntos del Terreno', 'puntos_del_terreno', 'khaki', 'Point'),
-    createVectorLayer('Salvado de Obstáculo', 'salvado_de_obstaculo', 'lavender', 'Point'),
-    createVectorLayer('Señalizaciones', 'senalizaciones', 'ivory', 'Point'),
+    createVectorLayer('Actividades Agropecuarias', 'actividades_agropecuarias', 'Point'),
+    createVectorLayer('Actividades Económicas', 'actividades_economicas', 'Point'),
+    createVectorLayer('Complejo de Energía', 'complejo_de_energia', 'Point'),
+    createVectorLayer('Edif Construcciones Turísticas', 'edif_construcciones_turisticas', 'Point'),
+    createVectorLayer('Edif Depor y Esparcimiento', 'edif_depor_y_esparcimiento', 'Point'),
+    createVectorLayer('Edif Educación', 'edif_educacion', 'Point'),
+    createVectorLayer('Edif Religiosos', 'edif_religiosos', 'Point'),
+    createVectorLayer('Edificio de Seguridad', 'edificio_de_seguridad', 'Point'),
+    createVectorLayer('Edificio Público', 'edificio_publico', 'Point'),
+    createVectorLayer('Edificios Ferroviarios', 'edificios_ferroviarios', 'Point'),
+    createVectorLayer('Edificio de Salud', 'edificio_de_salud', 'Point'),
+    createVectorLayer('Estructuras Portuarias', 'estructuras_portuarias', 'Point'),
+    createVectorLayer('Infraest Aeroportuaria Punto', 'infraest_aeroportuaria_punto', 'Point'),
+    createVectorLayer('Infraestructura Hidro', 'infraestructura_hidro', 'Point'),
+    createVectorLayer('Localidad', 'localidad', 'Point'),
+    createVectorLayer('Marcas y Señales', 'marcas_y_senales', 'Point'),
+    createVectorLayer('Obra Portuaria', 'obra_portuaria', 'Point'),
+    createVectorLayer('Otras Edificaciones', 'otras_edificaciones', 'Point'),
+    createVectorLayer('Puente Red Vial Punto', 'puente_red_vial_punto', 'Point'),
+    createVectorLayer('Puntos de Alturas Topográficas', 'puntos_de_alturas_topograficas', 'Point'),
+    createVectorLayer('Puntos del Terreno', 'puntos_del_terreno', 'Point'),
+    createVectorLayer('Salvado de Obstáculo', 'salvado_de_obstaculo', 'Point'),
+    createVectorLayer('Señalizaciones', 'senalizaciones', 'Point'),
   ];
 
   const lineLayers = [
-    createVectorLayer('Curso de Agua', 'curso_de_agua', 'blue', 'LineString'),
-    createVectorLayer('Curvas de Nivel', 'curvas_de_nivel', 'brown', 'LineString'),
-    createVectorLayer('Límite Político Administrativo', 'limite_politico_administrativo', 'red', 'LineString'),
-    createVectorLayer('Líneas de Conducción de Energía', 'lineas_de_conduccion_ene', 'orange', 'LineString'),
-    createVectorLayer('Red Ferroviaria', 'red_ferroviaria', 'black', 'LineString'),
-    createVectorLayer('Red Vial', 'red_vial', 'yellow', 'LineString'),
-    createVectorLayer('Vías Secundarias', 'vias_secundarias', 'green', 'LineString'),
+    createVectorLayer('Curso de Agua', 'curso_de_agua', 'LineString'),
+    createVectorLayer('Curvas de Nivel', 'curvas_de_nivel', 'LineString'),
+    createVectorLayer('Límite Político Administrativo', 'limite_politico_administrativo', 'LineString'),
+    createVectorLayer('Líneas de Conducción de Energía', 'lineas_de_conduccion_ene', 'LineString'),
+    createVectorLayer('Red Ferroviaria', 'red_ferroviaria', 'LineString'),
+    createVectorLayer('Red Vial', 'red_vial', 'LineString'),
+    createVectorLayer('Vías Secundarias', 'vias_secundarias', 'LineString'),
   ];
 
   const polygonLayers = [
-    createVectorLayer('Ejido', 'ejido', 'blue', 'Polygon'),
-    createVectorLayer('Espejos de Agua', 'espejos_de_agua', 'cyan', 'Polygon'),
-    createVectorLayer('Isla', 'isla', 'green', 'Polygon'),
-    createVectorLayer('País', 'pais_lim', 'red', 'Polygon'),
-    createVectorLayer('Sue Congelado', 'sue_congelado', 'gray', 'Polygon'),
-    createVectorLayer('Sue Consolidado', 'sue_consolidado', 'brown', 'Polygon'),
-    createVectorLayer('Sue Costero', 'sue_costero', 'yellow', 'Polygon'),
-    createVectorLayer('Sue Hidromorfologico', 'sue_hidromorfologico', 'olive', 'Polygon'),
-    createVectorLayer('Sue No Consolidado', 'sue_no_consolidado', 'orange', 'Polygon'),
-    createVectorLayer('Veg Arborea', 'veg_arborea', 'darkgreen', 'Polygon'),
-    createVectorLayer('Veg Arbustiva', 'veg_arbustiva', 'lightgreen', 'Polygon'),
-    createVectorLayer('Veg Cultivos', 'veg_cultivos', 'green', 'Polygon'),
-    createVectorLayer('Veg Hidrofila', 'veg_hidrofila', 'blue', 'Polygon'),
-    createVectorLayer('Veg Suelo Desnudo', 'veg_suelo_desnudo', 'tan', 'Polygon'),
+    createTileLayer('Ejido', 'ejido'),
+    createTileLayer('Espejos de Agua', 'espejos_de_agua'),
+    createTileLayer('Isla', 'isla'),
+    createTileLayer('País', 'pais_lim'),
+    createTileLayer('Sue Congelado', 'sue_congelado'),
+    createTileLayer('Sue Consolidado', 'sue_consolidado'),
+    createTileLayer('Sue Costero', 'sue_costero'),
+    createTileLayer('Sue Hidromorfologico', 'sue_hidromorfologico'),
+    createTileLayer('Sue No Consolidado', 'sue_no_consolidado'),
+    createTileLayer('Veg Arborea', 'veg_arborea'),
+    createTileLayer('Veg Arbustiva', 'veg_arbustiva'),
+    createTileLayer('Veg Cultivos', 'veg_cultivos'),
+    createTileLayer('Veg Hidrofila', 'veg_hidrofila'),
+    createTileLayer('Veg Suelo Desnudo', 'veg_suelo_desnudo'),
   ];
 
   return [...pointLayers, ...lineLayers, ...polygonLayers];
--- tp-final/src/componentes/Legend.js
+++ tp-final/src/componentes/Legend.js
@@ -1,67 +1,33 @@
 // Legend.js
 import React, { useEffect, useState } from 'react';
-import ImageWMS from 'ol/source/ImageWMS';
 import './Legend.css';
 
 const Legend = ({ layers }) => {
-  const [legendUrl, setLegendUrl] = useState('');
+  const [legendItems, setLegendItems] = useState([]);
 
   useEffect(() => {
-    const activeLayer = layers.find(layer => layer.getVisible() && layer.getSource() instanceof ImageWMS);
-    if (!activeLayer) return;
-
-    const wmsSource = activeLayer.getSource();
-
-    const updateLegend = (resolution) => {
-      const graphicUrl = wmsSource.getLegendUrl(resolution);
-      setLegendUrl(graphicUrl);
-    };
-
-    // Initial legend
-    const resolution = activeLayer.get('map').getView().getResolution();
-    updateLegend(resolution);
-
-    // Update the legend when the resolution changes
-    const view = activeLayer.get('map').getView();
-    const handleResolutionChange = () => {
-      const resolution = view.getResolution();
-      updateLegend(resolution);
-    };
-    view.on('change:resolution', handleResolutionChange);
-
-    // Cleanup event listener on unmount
-    return () => {
-      view.un('change:resolution', handleResolutionChange);
-    };
+    const visibleLayers = layers.filter(layer => layer.getVisible());
+    const legendItems = visibleLayers.map(layer => {
+      const styleUrl = layer.get('styleUrl');
+      const iconSrc = layer.get('iconSrc');
+      return { title: layer.get('title'), styleUrl, iconSrc };
+    });
+    setLegendItems(legendItems);
   }, [layers]);
 
-  const visibleLayers = layers.filter(layer => layer.getVisible());
-
-  if (visibleLayers.length === 0) {
-    return null;
+  if (legendItems.length === 0) {
+    return null; // No renderizar nada si no hay capas visibles
   }
-
-  const activeLayer = layers.find(layer => layer.get('active'));
 
   return (
     <div className="legend-section">
-      <h3>Leyenda</h3>
-      {activeLayer && (
-        <div className="active-layer">
-          <h4>Active Layer</h4>
-          <p>Title: {activeLayer.get('title')}</p>
-          <img src={activeLayer.get('styleUrl')} alt="Layer Style" />
+      {legendItems.map((item, index) => (
+        <div key={index} className="legend-item">
+          {item.iconSrc && <img src={item.iconSrc} alt={item.title} className="legend-icon" />}
+          {item.styleUrl && <img src={item.styleUrl} alt={item.title} className="legend-style" />}
+          <span>{item.title}</span>
         </div>
-      )}
-      <ul className="legend-list">
-        {visibleLayers.map((layer, index) => (
-          <li key={index} className="legend-item">
-            <img src={layer.get('styleUrl')} alt="Layer Style" className="legend-symbol" />
-            <span className="legend-title">{layer.get('title')}</span>
-          </li>
-        ))}
-      </ul>
-      {legendUrl && <img id="legend" src={legendUrl} alt="WMS Legend" />}
+      ))}
     </div>
   );
 };
